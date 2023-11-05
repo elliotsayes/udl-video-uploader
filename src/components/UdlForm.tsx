7f7204ee37e7a443245334c1080d5a409467fd7c
Elliot Sayes <elliot@sayes.cloud>
2023-11-05 19:25:29 +0700

    UDL form

---
+++ src/components/UdlForm.tsx
@@ -0,0 +1,208 @@
+import { zCommercialUse, zDerivations, zLicenseFeeCurrency, zLicenseType, zUdlInputSchema } from "@/types/udl"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
+import { useState } from "react"
+import { ResetIcon } from "@radix-ui/react-icons"
+
+export const UdlForm = () => {
+  const form = useForm<z.infer<typeof zUdlInputSchema>>({
+    resolver: zodResolver(zUdlInputSchema),
+    defaultValues: {
+      Derivations: 'Unspecified',
+      "Commercial Use": 'Unspecified',
+      "License Type": 'Unspecified',
+      "License Fee Value": undefined,
+      "License Fee Currency": "$U",
+      "Revenue Share Percentage": undefined,
+      Expires: undefined,
+      "Payment Address": '',
+      // "Payment Mode": 'Unspecified',
+    },
+  })
+
+  const [isRevenueShare, setIsRevenueShare] = useState(false)
+  const [isLicense, setIsLicense] = useState(false)
+
+  form.watch((value) => {
+    value["Derivations"] === 'Allowed-With-RevenueShare' ? setIsRevenueShare(true) : setIsRevenueShare(false)
+    value["License Type"] !== "Unspecified" ? setIsLicense(true) : setIsLicense(false)
+  })
+
+  function onSubmit(values: z.infer<typeof zUdlInputSchema>) {
+    console.log(values)
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const renderSelect = (field: any, title: string, description: string, options: string[], firstOption?: string) => {
+    return (
+      <FormItem className="pt-8">
+        <FormLabel>{title}</FormLabel>
+        <Select onValueChange={field.onChange} defaultValue={field.value}>
+          <FormControl>
+            <SelectTrigger>
+              <SelectValue />
+            </SelectTrigger>
+          </FormControl>
+          <SelectContent>
+            <SelectContent />
+              { 
+                firstOption && <SelectItem key={firstOption} value={firstOption}>{firstOption}</SelectItem>
+              }
+              {
+                options.map((option) => {
+                  return <SelectItem key={option} value={option}>{option}</SelectItem>
+                })
+              }
+          </SelectContent>
+        </Select>
+        <FormDescription>
+          You can manage email addresses...
+        </FormDescription>
+        <FormMessage />
+      </FormItem>
+    )
+  }
+
+  const rootErrorMessage = form.formState.errors[""]?.["message"] as string | undefined;
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <div className="flex flex-col">
+          <div className="flex flex-col md:flex-row md:gap-8">
+            <FormField
+              control={form.control}
+              name={"Derivations"}
+              render={({ field }) => renderSelect(field, 'Derivations', '<description>', zDerivations.options, 'Unspecified')}
+            />
+            <div className={`flex ${isRevenueShare ? 'h-32 pt-8 opacity-100' : 'h-0 opacity-0 overflow-hidden'} transition-all duration-200`}>
+              <FormField
+                control={form.control}
+                name={"Revenue Share Percentage"}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Revenue Share Percentage</FormLabel>
+                    <FormControl>
+                      <div className="flex flex-row items-center">
+                        <Input placeholder="10" {...field} />
+                        <span className="pl-2">%</span>
+                      </div>
+                    </FormControl>
+                    <FormDescription>
+                      This is your public display name.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+          </div>
+          <FormField
+            control={form.control}
+            name={"Commercial Use"}
+            render={({ field }) => renderSelect(field, 'Commercial Use', '<desc>', zCommercialUse.options, 'Unspecified')}
+          />
+          <FormField
+            control={form.control}
+            name={"License Type"}
+            render={({ field }) => renderSelect(field, 'License Type', '<description>', zLicenseType.options, 'Unspecified')}
+          />
+          <div className={`flex flex-col md:flex-row md:gap-8 ${isLicense ? 'h-72 md:h-36 opacity-100' : 'h-0 opacity-0 overflow-hidden'} transition-all duration-200`}>
+            <FormField
+              control={form.control}
+              name={"License Fee Value"}
+              render={({ field }) => (
+                <FormItem className="pt-8">
+                  <FormLabel>License Fee Value</FormLabel>
+                  <FormControl>
+                    <Input placeholder="10" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    This is your public display name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name={"License Fee Currency"}
+              render={({ field }) => renderSelect(field, 'License Fee Currency', '<description>', zLicenseFeeCurrency.options, '$U')}
+            />
+          </div>
+          <div className={`${isRevenueShare || isLicense ? 'h-36 pt-8 opacity-100' : 'h-0 opacity-0 overflow-hidden'} transition-all duration-200`}>
+            <FormField
+              control={form.control}
+              name={"Payment Address"}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Payment Address</FormLabel>
+                  <FormControl>
+                    <Input placeholder="0cQJ..." {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    Optional, uses your wallet address if not specified.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            {/* <FormField
+              control={form.control}
+              name={"Payment Mode"}
+              render={({ field }) => renderSelect(field, 'Payment Mode', '<description>', zPaymentMode.options, 'Unspecified')}
+            /> */}
+          </div>
+          <FormField
+            control={form.control}
+            name={"Expires"}
+            render={({ field }) => (
+              <FormItem className="pt-8">
+                <FormLabel>Expires</FormLabel>
+                <FormControl>
+                  <Input placeholder="5" {...field} />
+                </FormControl>
+                <FormDescription>
+                  Number of years the license is valid for (optional)
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="my-4 h-4">
+            <FormMessage>
+              {rootErrorMessage}
+            </FormMessage>
+          </div>
+          <div className="flex flex-row justify-between items-center gap-4">
+            <Button
+              size={"icon"}
+              className="invisible"
+            />
+            <Button
+              type="submit"
+              variant={"default"}
+              size={"lg"}
+            >
+              Submit
+            </Button>
+            <Button
+              onClick={() => form.reset()}
+              disabled={!form.formState.isDirty}
+              variant={"destructive"}
+              size={"icon"}
+              aria-description="Reset form"
+            >
+              <ResetIcon />
+            </Button>
+          </div>
+        </div>
+      </form>
+    </Form>
+  )
+}
---
+++ src/types/udl.ts
@@ -0,0 +1,93 @@
+import { z } from "zod";
+
+export const zDerivations = z.enum([
+  "Allowed-With-Credit",
+  "Allowed-With-Indication",
+  "Allowed-With-License-Passthrough",
+  "Allowed-With-RevenueShare",
+]);
+
+export const zRevenueSharePercentage = z
+  .number()
+  .gt(0, {
+    message: "Revenue share must be greater than 0",
+  })
+  .max(100, {
+    message: "Revenue share may not be greater than 100",
+  });
+
+export const zCommercialUse = z.enum(["Allowed", "Allowed-With-Credit"]);
+
+export const zLicenseType = z.enum(["Monthly", "One Time"]);
+
+export const zLicenseFeeValue = z.number().gt(0, {
+  message: "License fee must be greater than 0",
+});
+
+export const zLicenseFeeCurrency = z.enum(["AR"]);
+
+export const zExpires = z
+  .number()
+  .int({
+    message: "Expires must be a whole number of years",
+  })
+  .gt(0, {
+    message: "Expires must be greater than 0",
+  });
+
+export const zPaymentAddress = z.string().min(1, {
+  message: "Payment address cannot be empty",
+});
+
+export const zPaymentMode = z.enum([
+  "Random-Distribution",
+  "Global-Distribution",
+]);
+
+export const zUnspecified = z.enum(["Unspecified"]);
+
+export const zUdlInputSchema = z
+  .object({
+    Derivations: z.union([zUnspecified, zDerivations]),
+    "Revenue Share Percentage": z.union([
+      z.coerce.number().pipe(zRevenueSharePercentage),
+      z.undefined(),
+      z.string().length(0),
+    ]),
+    "Commercial Use": z.union([zUnspecified, zCommercialUse]),
+    "License Type": z.union([zUnspecified, zLicenseType]),
+    "License Fee Value": z.union([
+      z.coerce.number().pipe(zLicenseFeeValue),
+      z.undefined(),
+      z.string().length(0),
+    ]),
+    "License Fee Currency": z.union([z.enum(["$U"]), zLicenseFeeCurrency]),
+    Expires: z.union([
+      z.coerce.number().pipe(zExpires),
+      z.undefined(),
+      z.string().length(0),
+    ]),
+    "Payment Address": z.union([z.string().length(0), zPaymentAddress]),
+    // "Payment Mode": z.union([zUnspecified, zPaymentMode]),
+  })
+  .refine(
+    (data) =>
+      !(
+        data["Derivations"] === "Allowed-With-RevenueShare" &&
+        typeof data["Revenue Share Percentage"] != "number"
+      ),
+    {
+      message:
+        "RevenueShare Percentage is required when derivation is Allowed-With-RevenueShare",
+    }
+  )
+  .refine(
+    (data) =>
+      !(
+        data["License Type"] !== "Unspecified" &&
+        typeof data["License Fee Value"] != "number"
+      ),
+    {
+      message: "Must have License Fee Value when License Type is set",
+    }
+  );
