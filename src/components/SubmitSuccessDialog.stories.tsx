e1b4ebd9d7cabcf1de2d793cee6d8813daaf8aa9
Elliot Sayes <elliot@sayes.cloud>
2023-11-06 19:42:01 +0700

    Better examples in story

--- src/components/SubmitSuccessDialog.stories.tsx
+++ src/components/SubmitSuccessDialog.stories.tsx
@@ -28,31 +28,45 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-const exampleVideoResult = {
-  "everHash": "0x6213fe7c826a499162cf6785eb9455296e3a167b6bc959ff17d973a1ec2a2541",
+const mainVideoResult = {
+  "everHash": "0x03f1c87393a869f6bb19175c4badee520485d41b0c2bfe10f593b835531c5b60",
   "order": {
-    "itemId": "gvOd2uF-yImgp74P7IzOke_555m-Xubjpr_97SX9B20",
-    "size": 2071835,
-    "bundler": "uDA8ZblC-lyEFfsYXKewpwaX-kkNDDw8az3IW9bDL68",
-    "currency": "AR",
-    "decimals": 12,
-    "fee": "1678189238",
-    "paymentExpiredTime": 1701839600,
-    "expectedBlock": 1296717,
-    "tag": "arweave,ethereum-ar-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA,0x4fadc7a98f2dc96510e42dd1a74141eeae0c1543"
+      "itemId": "Ff6hyvaICMBnYyADpZLkvDj69LzraxR2XWl5BdOlvHI",
+      "size": 5304352,
+      "bundler": "uDA8ZblC-lyEFfsYXKewpwaX-kkNDDw8az3IW9bDL68",
+      "currency": "AR",
+      "decimals": 12,
+      "fee": "4401078966",
+      "paymentExpiredTime": 1701861248,
+      "expectedBlock": 1296889,
+      "tag": "arweave,ethereum-ar-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA,0x4fadc7a98f2dc96510e42dd1a74141eeae0c1543"
   }
-}
+};
+const trailerVideoResult = {
+  "everHash": "0x1e70be86389dc6435c5239b1ece8e71fb0b95fc518c0cc419832190f1c311530",
+  "order": {
+      "itemId": "SoN2tDbptfy4B7sY6Gw-WvU_yC3UuWHfyfPb6Qlhhkg",
+      "size": 4610675,
+      "bundler": "uDA8ZblC-lyEFfsYXKewpwaX-kkNDDw8az3IW9bDL68",
+      "currency": "AR",
+      "decimals": 12,
+      "fee": "3772719798",
+      "paymentExpiredTime": 1701861238,
+      "expectedBlock": 1296889,
+      "tag": "arweave,ethereum-ar-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA,0x4fadc7a98f2dc96510e42dd1a74141eeae0c1543"
+  }
+};
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const MainVideoOnly: Story = {
   args: {
-    mainVideoResult: exampleVideoResult,
+    mainVideoResult,
   },
 };
 
 export const MainVideoAndTrailer: Story = {
   args: {
-    mainVideoResult: exampleVideoResult,
-    trailerVideoResult: exampleVideoResult,
+    mainVideoResult,
+    trailerVideoResult,
   },
 };
