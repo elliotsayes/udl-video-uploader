ddbb67b02291e308182210e1c5b2c439755a1a69
Elliot Sayes <elliot@sayes.cloud>
2023-11-09 15:40:03 +0700

    Code "arseeding" into config file

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,9 @@ export const config = {
   arseedingUrl: import.meta.env.VITE_CONFIG_ARSEEDING_URL,
   arseedingDefaultSymbol: import.meta.env.VITE_CONFIG_ARSEEDING_DEFAULT_SYMBOL,
   bundlrNode: import.meta.env.VITE_CONFIG_BUNDLR_NODE,
-  uploader: import.meta.env.VITE_CONFIG_UPLOADER as "arseeding" | "bundlr",
+  uploader: (import.meta.env.VITE_CONFIG_UPLOADER ?? "arseeding") as
+    | "arseeding"
+    | "bundlr",
   rendererArns: import.meta.env.VITE_CONFIG_RENDERER_ARNS as string,
   defaultRendererTxId: import.meta.env.VITE_CONFIG_RENDERER_TX_ID as
     | string
