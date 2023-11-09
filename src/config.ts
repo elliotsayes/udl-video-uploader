8ae174890f86d76bdf4fc2194b95099b400f2259
Elliot Sayes <elliot@sayes.cloud>
2023-11-09 14:12:27 +0700

    configure arns

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,7 @@ export const config = {
   arseedingDefaultSymbol: import.meta.env.VITE_CONFIG_ARSEEDING_DEFAULT_SYMBOL,
   bundlrNode: import.meta.env.VITE_CONFIG_BUNDLR_NODE,
   uploader: import.meta.env.VITE_CONFIG_UPLOADER as "arseeding" | "bundlr",
+  rendererArns: import.meta.env.VITE_CONFIG_RENDERER_ARNS,
   rendererTxId: import.meta.env.VITE_CONFIG_RENDERER_TX_ID as
     | string
     | undefined,
